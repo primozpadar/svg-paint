2e117d387b2f097d830f4e956a0a27b55fb020b1
primozpadar <primoz.padar1@gmail.com>
2021-08-14 20:15:07 +0200

    color picker

--- src/main.ts
+++ src/main.ts
@@ -3,28 +3,34 @@ import { Save } from './save';
 import { Line } from './svg/line';
 import { Rectangle } from './svg/rectangle';
 import { Svg } from './svg/svg';
+import { ColorsBar } from './toolbar/colorsBar';
 import { Toolbar } from './toolbar/toolbar';
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
 // main svg element
-const bgColor = 'transparent';
 const svg = new Svg(app, { height: '100vh', width: '100vw' });
-svg.setBackgroundColor(bgColor);
 
 const toolbar = new Toolbar(app);
+const colorsBar = new ColorsBar(app);
+colorsBar.addPicker('Background', 'background', (color) => {
+  svg.setBackgroundColor(color);
+});
+colorsBar.addPicker('Stoke', 'stroke');
+
+const getStrokeStyle = () => ({ color: ColorsBar.colors.stroke, width: 1 });
 
 let tempLine: Line;
 toolbar.addDrawButton('Line', svg, {
-  onStart: (p1) => (tempLine = new Line(svg, p1, p1)),
+  onStart: (p1) => (tempLine = new Line(svg, p1, p1, { stroke: getStrokeStyle() })),
   onMove: (p2) => tempLine.updatePoints({ p2 }),
 });
 
 let tempRect: Rectangle;
 toolbar.addDrawButton('Rectangle', svg, {
-  onStart: (p1) => (tempRect = new Rectangle(svg, p1, p1)),
+  onStart: (p1) => (tempRect = new Rectangle(svg, p1, p1, { stroke: getStrokeStyle() })),
   onMove: (p2) => tempRect.updatePoints({ p2 }),
 });
 
-toolbar.addButton('Screenshot', () => Save.screenshot(svg, bgColor));
+toolbar.addButton('Screenshot', () => Save.screenshot(svg, ColorsBar.colors.background));
 toolbar.addButton('Save SVG', () => Save.svg(svg));
